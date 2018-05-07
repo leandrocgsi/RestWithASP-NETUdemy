8d16f9b53dd096d209e90809a7d7dcf42ff8c2a5
leandrocgsi <leandrocgsi@gmail.com>
2018-05-07 00:34:51 -0300

    Versioning my API

---
+++ RestWithASPNETUdemy 04 - Versioning Endpoints/RestWithASPNETUdemy/Model/Context/MySQLContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ RestWithASPNETUdemy 04 - Versioning Endpoints/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementattions;
+using RestWithASPNETUdemy.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+            services.AddMvc();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
