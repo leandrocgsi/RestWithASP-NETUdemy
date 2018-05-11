8181f8f95b71eacfa36c3693102684321306c8f1
leandrocgsi <leandrocgsi@gmail.com>
2018-05-11 09:53:28 -0300

    Adding evolve support

---
+++ RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Business/Implementattions/PersonBusinessImpl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImpl(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public bool Exists(long id)
+        {
+            return _repository.Exists(id);
+        }
+    }
+}
