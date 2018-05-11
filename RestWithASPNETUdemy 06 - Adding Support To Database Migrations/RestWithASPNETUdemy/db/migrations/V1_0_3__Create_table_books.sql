d4f39db53a9bfd08d4f57e4d94883570d3e5e53b
Leandro Costa <leandrocgsi@gmail.com>
2018-05-11 15:11:50 -0300

    Update V1_0_3__Create_table_books.sql

--- RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/db/migrations/V1_0_3__Create_table_books.sql
+++ RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/db/migrations/V1_0_3__Create_table_books.sql
@@ -2,7 +2,7 @@
   `id` varchar(127) NOT NULL,
   `Author` longtext,
   `LaunchDate` datetime(6) NOT NULL,
-  `Price` decimal(65,30) NOT NULL,
+  `Price` decimal(65,2) NOT NULL,
   `Title` longtext,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
