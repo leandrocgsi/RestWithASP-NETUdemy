050bc5d8c2d4e0316081a8e687c3ed29184a7aaf
leandrocgsi <leandrocgsi@gmail.com>
2018-05-13 02:14:46 -0300

    Working With Value Objects

---
+++ RestWithASPNETUdemy 08 - Working With Value Objects/RestWithASPNETUdemy/Data/VO/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class BookVO
+    {
+        public long? Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LaunchDate { get; set; }
+    }
+}
