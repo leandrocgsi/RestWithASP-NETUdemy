7cfb6923bb24ae9bcfc57c02176f5433e9aa7771
leandrocgsi <leandrocgsi@gmail.com>
2018-05-17 13:50:53 -0300

    Implementing HATEOAS

--- RestWithASPNETUdemy 11 - HATEOAS/HyperMedia/HyperMediaLink.cs
+++ RestWithASPNETUdemy 11 - HATEOAS/HyperMedia/HyperMediaLink.cs
@@ -57,10 +57,23 @@
         /// </summary>
         public string Rel { get; set; }
 
+        // Workarond for issues with slash encode https://github.com/aspnet/Home/issues/2669, https://github.com/aspnet/Routing/issues/363, https://github.com/aspnet/Home/issues/2669, https://github.com/aspnet/Routing/issues/363 and https://github.com/aspnet/Mvc/issues/2826
+        // Here is the ASP.NET Core 5 controller sample code: https://github.com/doggy8088/CatchAllRouteProblem/blob/master/MVC5/Controllers/HomeController.cs
+        // Here is the ASP.NET Core 2.0 controller sample code: https://github.com/doggy8088/CatchAllRouteProblem/blob/master/Core2MVC/Controllers/HomeController.cs
+        private string href;
+
         /// <summary>
         /// The hyperlink (Href) for the object being described by the link in the form of a URI
         /// </summary>
-        public string Href { get; set; }
+        public string Href
+        {
+            get {
+                return href.Replace("%2F", "/");
+            }
+            set {
+                href = value;
+            }
+        }
 
         /// <summary>
         /// The format of any data (Types) that should be provided in the HTTP request or that can be returned in the response, depending on the type of the request.
--- RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/HyperMedia/PersonEnricher.cs
+++ RestWithASPNETUdemy 11 - HATEOAS/RestWithASPNETUdemy/HyperMedia/PersonEnricher.cs
@@ -7,39 +7,36 @@ namespace RestWithASPNETUdemy.HyperMedia
 {
     public class PersonEnricher : ObjectContentResponseEnricher<PersonVO>
     {
-        //SEE: https://books.google.com.br/books?id=D54TAwAAQBAJ&pg=PA146&lpg=PA146&dq=Href+%3D+urlHelper.Link(%22DefaultApi%22&source=bl&ots=WuQj7v4KD9&sig=Ock40NqChWq0DZrs7thDImBsv4E&hl=pt-BR&sa=X&ved=0ahUKEwjT-7_u1orbAhVJgJAKHQJ7AZMQ6AEIVDAE#v=onepage&q=Href%20%3D%20urlHelper.Link(%22DefaultApi%22&f=false
-
         protected override Task EnrichModel(PersonVO content, IUrlHelper urlHelper)
         {
-            var path = "persons/v1";
-            //var path = "persons/v{version:apiVersion}";
+            var path = "api/persons/v1";
+            var url = new { controller = path, id = content.Id };
 
-            //https://blogs.msdn.microsoft.com/roncain/2012/07/17/using-the-asp-net-web-api-urlhelper/
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.GET,
-                Href = urlHelper.Link("DefaultApi", new { controller = path, id = content.Id }),
+                Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
                 Type = RensponseTypeFormat.DefaultGet
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.POST,
-                Href = urlHelper.Link("DefaultApi", new { controller = path, id = content.Id }),
+                Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
                 Type = RensponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PUT,
-                Href = urlHelper.Link("DefaultApi", new { controller = path, id = content.Id }),
+                Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
                 Type = RensponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.DELETE,
-                Href = urlHelper.Link("DefaultApi", new { controller = path, id = content.Id }),
+                Href = urlHelper.Link("DefaultApi", url),
                 Rel = RelationType.self,
                 Type = "int"
             });
