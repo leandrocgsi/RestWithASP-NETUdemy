c5102f91bd3b6ffde1fce57997ecb71a6e7e40b7
leandrocgsi <leandrocgsi@gmail.com>
2018-05-17 15:10:25 -0300

    Implementing authentication

---
+++ RestWithASPNETUdemy 13 - Authentication/HyperMedia/HyperMediaFilterOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HyperMedia
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ObjectContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
---
+++ RestWithASPNETUdemy 13 - Authentication/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,105 @@
+﻿using HyperMedia;
+using Microsoft.AspNetCore.Mvc;
+
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : Controller
+    {
+        private IBookBusiness _bookBusiness;
+
+        public BooksController(IBookBusiness bookBusiness)
+        {
+            _bookBusiness = bookBusiness;
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/books/v1/
+        // [SwaggerResponse((202), Type = typeof(List<Book>))]
+        // determina o objeto de retorno em caso de sucesso List<Book>
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 204, 400 e 401
+        [HttpGet]
+        [SwaggerResponse((200), Type = typeof(List<BookVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/books/v1/{id}
+        // [SwaggerResponse((202), Type = typeof(Book))]
+        // determina o objeto de retorno em caso de sucesso Book
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 204, 400 e 401
+        [HttpGet("{id}")]
+        [SwaggerResponse((200), Type = typeof(BookVO))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/
+        // [SwaggerResponse((202), Type = typeof(Book))]
+        // determina o objeto de retorno em caso de sucesso Book
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpPost]
+        [SwaggerResponse((201), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return new ObjectResult(_bookBusiness.Create(book));
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/books/v1/
+        // [SwaggerResponse((202), Type = typeof(Book))]
+        // determina o objeto de retorno em caso de sucesso Book
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpPut]
+        [SwaggerResponse((202), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updatedBook = _bookBusiness.Update(book);
+            if (updatedBook == null) return BadRequest();
+            return new ObjectResult(updatedBook);
+        }
+
+        // Configura o Swagger para a operação
+        // http://localhost:{porta}/api/books/v1/{id}
+        // O [SwaggerResponse(XYZ)] define os códigos de retorno 400 e 401
+        [HttpDelete("{id}")]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
