7f2a3d368e3fc8101f25d56fd9d6fc8f12d534d1
leandrocgsi <leandrocgsi@gmail.com>
2018-05-20 23:57:43 -0300

    Implementing authentication

--- RestWithASPNETUdemy 13 - Authentication/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy 13 - Authentication/RestWithASPNETUdemy/Startup.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Rewrite;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Net.Http.Headers;
+
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementattions;
+using RestWithASPNETUdemy.HyperMedia;
+using RestWithASPNETUdemy.Repository.Generic;
+using RestWithASPNETUdemy.Security.Configuration;
 
 using Swashbuckle.AspNetCore.Swagger;
 
 using Tapioca.HATEOAS;
-
-using RestWithASPNETUdemy.Business;
-using RestWithASPNETUdemy.Business.Implementattions;
-using RestWithASPNETUdemy.HyperMedia;
-using RestWithASPNETUdemy.Model.Context;
-using RestWithASPNETUdemy.Repository.Generic;
-using RestWithASPNETUdemy.Security.Configuration;
-using RestWithASPNETUdemy.Services.Implementattions;
 
 namespace RestWithASPNETUdemy
 {
@@ -44,9 +43,11 @@ namespace RestWithASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connection to database
             var connectionString = _configuration["MySqlConnection:MySqlConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connectionString));
 
+            //Adding Migrations Support
             if (_environment.IsDevelopment())
             {
                 try
@@ -93,20 +94,20 @@ namespace RestWithASPNETUdemy
                 paramsValidation.ValidAudience = tokenConfigurations.Audience;
                 paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
 
-                // Valida a assinatura de um token recebido
+                // Validates the signing of a received token
                 paramsValidation.ValidateIssuerSigningKey = true;
 
-                // Verifica se um token recebido ainda é válido
+                // Checks if a received token is still valid
                 paramsValidation.ValidateLifetime = true;
 
-                // Tempo de tolerância para a expiração de um token (utilizado
-                // caso haja problemas de sincronismo de horário entre diferentes
-                // computadores envolvidos no processo de comunicação)
+                // Tolerance time for the expiration of a token (used in case
+                // of time synchronization problems between different
+                // computers involved in the communication process)
                 paramsValidation.ClockSkew = TimeSpan.Zero;
             });
 
-            // Ativa o uso do token como forma de autorizar o acesso
-            // a recursos deste projeto
+            // Enables the use of the token as a means of
+            // authorizing access to this project's resources
             services.AddAuthorization(auth =>
             {
                 auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
@@ -114,6 +115,7 @@ namespace RestWithASPNETUdemy
                     .RequireAuthenticatedUser().Build());
             });
 
+            //Content negociation - Support to XML and JSON
             services.AddMvc(options =>
             {
                 options.RespectBrowserAcceptHeader = true;
@@ -123,14 +125,19 @@ namespace RestWithASPNETUdemy
             })
             .AddXmlSerializerFormatters();
 
-            var filtertOptions = new HyperMediaFilterOptions();
-            filtertOptions.ObjectContentResponseEnricherList.Add(new BookEnricher());
-            filtertOptions.ObjectContentResponseEnricherList.Add(new PersonEnricher());
-            services.AddSingleton(filtertOptions);
+            //HATEOAS filter definitions
+            var filterOptions = new HyperMediaFilterOptions();
+            filterOptions.ObjectContentResponseEnricherList.Add(new BookEnricher());
+            filterOptions.ObjectContentResponseEnricherList.Add(new PersonEnricher());
 
+            //Service inject
+            services.AddSingleton(filterOptions);
+
+            //Versioning
             services.AddApiVersioning(option => option.ReportApiVersions = true);
 
-            services.AddSwaggerGen(c => 
+            //Add Swagger Service
+            services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1",
                     new Info
@@ -144,7 +151,8 @@ namespace RestWithASPNETUdemy
             services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
             services.AddScoped<IBookBusiness, BookBusinessImpl>();
 
-            services.AddScoped<IUserRepository, UserRepositoryImpl>();
+            // Não precisamos mais fazer dessa forma
+            //services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
 
             //Dependency Injection of GenericRepository
             services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
@@ -156,16 +164,19 @@ namespace RestWithASPNETUdemy
             loggerFactory.AddConsole(_configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            //Enable Swagger
             app.UseSwagger();
 
             app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
+            //Starting our API in Swagger page
             var option = new RewriteOptions();
             option.AddRedirect("^$", "swagger");
             app.UseRewriter(option);
 
+            //Adding map routing
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
