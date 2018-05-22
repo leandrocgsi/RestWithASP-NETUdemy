47a777d789b1186a5ec979b63bca0be7cd8e72e3
leandrocgsi <leandrocgsi@gmail.com>
2018-05-22 17:09:14 -0300

    Adding Docker Support

---
+++ RestWithASPNETUdemy Habilitando Docker/RestWithASPNETUdemy/Business/Implementattions/FileBusinessImpl.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class FileBusinessImpl : IFileBusiness
+    {
+        public byte[] GetPDFFile()
+        {
+            string path = Directory.GetCurrentDirectory();
+            var fullPath = path + "\\Other\\aspnet-life-cycles-events.pdf";
+            byte[] bytes = File.ReadAllBytes(fullPath);
+            return bytes;
+        }
+    }
+}
---
+++ RestWithASPNETUdemy Habilitando Docker/RestWithASPNETUdemy/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Business;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class LoginController : Controller
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public LoginController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post([FromBody]User user)
+        {
+            return _loginBusiness.FindByLogin(user);
+        }
+    }
+}
