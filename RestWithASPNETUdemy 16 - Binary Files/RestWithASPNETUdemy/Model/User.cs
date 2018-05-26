e5c4864fe5b6f49571738c54b92556528858a986
leandrocgsi <leandrocgsi@gmail.com>
2018-05-26 18:42:24 -0300

    Working with file download

---
+++ RestWithASPNETUdemy 16 - Binary Files/RestWithASPNETUdemy/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class User
+    {
+        public long? Id { get; set; }
+        public string Login { get; set; }
+        public string AccessKey { get; set; }
+    }
+}
