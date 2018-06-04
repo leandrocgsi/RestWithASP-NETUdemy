23456229e316ba2514b8359f7a15e6fed419058b
leandrocgsi <leandrocgsi@gmail.com>
2018-06-04 08:18:50 -0300

    Enable Docker support

---
+++ RestWithASPNETUdemy 17 - Enable Docker/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LaunchDate { get; set; }
+    }
+}
