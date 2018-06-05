b651f4f0afed3e74ca197e32edbad80a7a647d9a
leandrocgsi <leandrocgsi@gmail.com>
2018-06-05 10:33:19 -0300

    Adding Support To Database Migrations

--- RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Model/Person.cs
+++ RestWithASPNETUdemy 06 - Adding Support To Database Migrations/RestWithASPNETUdemy/Model/Person.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestWithASPNETUdemy.Model
 {
+	[Table("persons")]
     public class Person
     {
+		[Column("Id")]
         public long? Id { get; set; }
+
+		[Column("FirstName")]
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        
+        [Column("LastName")]
+		public string LastName { get; set; }
+
+        [Column("Address")]
         public string Address { get; set; }
+
+        [Column("Gender")]
         public string Gender { get; set; }
     }
 }
