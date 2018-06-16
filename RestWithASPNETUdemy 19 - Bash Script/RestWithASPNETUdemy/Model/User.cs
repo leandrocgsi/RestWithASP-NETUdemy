a6b60f58dda0409d5da687a6520f5f1330c4481c
leandrocgsi <leandrocgsi@gmail.com>
2018-06-16 15:27:36 -0300

    Starting database with bashscript

---
+++ RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+	[Table("users")]
+    public class User
+    {
+
+        public long? Id { get; set; }
+        public string Login { get; set; }
+        public string AccessKey { get; set; }
+    }
+}
