275a57767eab9fb5e096d5131b566126b576c35c
leandrocgsi <leandrocgsi@gmail.com>
2018-06-18 08:54:04 -0300

    Changing CI script

--- RestWithASPNETUdemy 17 - Enable Docker/RestWithASPNETUdemy/ci/init_database.sh
+++ RestWithASPNETUdemy 17 - Enable Docker/RestWithASPNETUdemy/ci/init_database.sh
@@ -1,6 +1,6 @@
-mkdir /home/database/all_scripts;
-chmod 777 /home/database/all_scripts;
-cp -a /home/database/dataset/. /home/database/all_scripts/;
-cp -a /home/database/migrations/. /home/database/all_scripts/;
-for i in `find /home/database/ -name "*.sql" | sort --version-sort`; do mysql -udocker -pdocker rest_with_asp_net_udemy < $i; done;
-rm -rf /home/database/all_scripts/;
+# mkdir /home/database/all_scripts;
+# chmod -R 777 /home/database/all_scripts;
+cp -a /home/database/dataset/*.sql /tmp/
+cp -a /home/database/migrations/*.sql /tmp/
+for i in `find /tmp/ -name "*.sql" | sort --version-sort`; do mysql -udocker -pdocker rest_with_asp_net_udemy < $i; done;
+# rm -rf /home/database/all_scripts/;
