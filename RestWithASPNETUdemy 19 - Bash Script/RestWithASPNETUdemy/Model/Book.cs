ffea73efa7e308ad706dd520a10e30ac974c3bab
leandrocgsi <leandrocgsi@gmail.com>
2018-06-17 23:56:05 -0300

    Implementing restore database by image

--- RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/Book.cs
+++ RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/Book.cs
@@ -7,16 +7,9 @@ namespace RestWithASPNETUdemy.Model
     [Table("books")]
     public class Book : BaseEntity
     {
-        [Column("Title")]
         public string Title { get; set; }
-
-        [Column("Author")]
         public string Author { get; set; }
-
-        [Column("Price")]
         public decimal Price { get; set; }
-
-        [Column("LaunchDate")]
         public DateTime LaunchDate { get; set; }
     }
 }
--- RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/Person.cs
+++ RestWithASPNETUdemy 19 - Bash Script/RestWithASPNETUdemy/Model/Person.cs
@@ -6,16 +6,16 @@ namespace RestWithASPNETUdemy.Model
     [Table("persons")]
     public class Person : BaseEntity
     {
-        [Column("FirstName")]
+        //[Column("FirstName")]
         public string FirstName { get; set; }
 
-        [Column("LastName")]
+        //[Column("LastName")]
         public string LastName { get; set; }
 
-        [Column("Address")]
+        //[Column("Address")]
         public string Address { get; set; }
 
-        [Column("Gender")]
+        //[Column("Gender")]
         public string Gender { get; set; }
     }
 }
