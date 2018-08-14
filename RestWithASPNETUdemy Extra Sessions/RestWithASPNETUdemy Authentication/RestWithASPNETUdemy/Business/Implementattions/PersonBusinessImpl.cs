6123460f329ad67093209c3ac31f26fa61ade0db
leandrocgsi <leandrocgsi@gmail.com>
2018-08-14 11:05:36 -0300

    Retrieve ClaimsPrincipal from JWT

---
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy Authentication/RestWithASPNETUdemy/Business/Implementattions/PersonBusinessImpl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository.Generic;
+using RestWithASPNETUdemy.Data.Converters;
+using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+
+        private IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImpl(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+        }
+
+        public PersonVO Create(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Create(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return _converter.ParseList(_repository.FindAll());
+        }
+
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public bool Exists(long id)
+        {
+            return _repository.Exists(id);
+        }
+    }
+}
