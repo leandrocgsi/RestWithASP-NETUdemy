919a4107d649a15d1c18bb54d380ead665cf8424
leandrocgsi <leandrocgsi@gmail.com>
2018-08-18 17:01:41 -0300

    HATEOAS Done On The Nail

---
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy HATEOAS Done On The Nail/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using Hypermedia.Filters;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : Controller
+    {
+        private IBookBusiness _bookBusiness;
+
+        public BooksController(IBookBusiness bookBusiness)
+        {
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        //Add HyperMediaFilter
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        //Add HyperMediaFilter
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return new OkObjectResult(book);
+        }
+
+        [HttpPost]
+        //Add HyperMediaFilter
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return new OkObjectResult(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        //Add HyperMediaFilter
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updatedBook = _bookBusiness.Update(book);
+            if (updatedBook == null) return BadRequest();
+            return new OkObjectResult(updatedBook);
+        }
+
+        [HttpDelete("{id}")]
+        //Add HyperMediaFilter
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
