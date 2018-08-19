a709f66bab5e484fc083018d2ba458025e2a66db
leandrocgsi <leandrocgsi@gmail.com>
2018-08-19 17:02:25 -0300

    HATEOAS Done On The Nail

---
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy HATEOAS Done On The Nail/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,17 @@
+﻿using Hypermedia;
+using Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO : ISupportsHyperMedia
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
