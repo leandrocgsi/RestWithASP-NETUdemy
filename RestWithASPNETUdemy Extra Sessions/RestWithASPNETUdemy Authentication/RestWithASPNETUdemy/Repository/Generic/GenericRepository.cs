91e207e5b21e1e0c188212db01e7b7176fdfc1f0
leandrocgsi <leandrocgsi@gmail.com>
2018-09-06 08:47:50 -0300

    Get user principal

--- RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy Authentication/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy Authentication/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
@@ -12,7 +12,6 @@ namespace RestWithASPNETUdemy.Repository.Generic
 
         private readonly MySQLContext _context;
 
-        // Declaração de um dataset genérico
         private DbSet<T> dataset;
 
         public GenericRepository(MySQLContext context)
