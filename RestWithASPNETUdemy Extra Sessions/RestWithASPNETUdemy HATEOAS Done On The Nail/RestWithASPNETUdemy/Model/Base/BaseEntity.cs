4e72336cb585c560775207c32786a27ab511820a
leandrocgsi <leandrocgsi@gmail.com>
2018-09-18 14:11:11 -0300

    Get user principal

--- RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy HATEOAS Done On The Nail/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy HATEOAS Done On The Nail/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -3,9 +3,6 @@ using System.Runtime.Serialization;
 
 namespace RestWithASPNETUdemy.Model.Base
 {
-    // Contrato entre atributos
-    // e a estrutura da tabela
-    // [DataContract]
     public class BaseEntity
     {
         [Column("id")]
