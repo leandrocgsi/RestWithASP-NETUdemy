a2d35d0076ca5fbcc29d284100a70f2fbee3f439
leandrocgsi <leandrocgsi@gmail.com>
2018-10-19 15:15:03 -0300

    Authentication Get User Principal

---
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy Authentication Get User Principal/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,87 @@
+﻿using Tapioca.HATEOAS;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : Controller
+    {
+        private IBookBusiness _bookBusiness;
+
+        public BooksController(IBookBusiness bookBusiness)
+        {
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        [SwaggerResponse((200), Type = typeof(List<BookVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerResponse((200), Type = typeof(BookVO))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return new OkObjectResult(book);
+        }
+
+		[HttpPost]
+        [SwaggerResponse((201), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return new OkObjectResult(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        [SwaggerResponse((202), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody]BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updatedBook = _bookBusiness.Update(book);
+            if (updatedBook == null) return BadRequest();
+            return new OkObjectResult(updatedBook);
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithASPNETUdemy Extra Sessions/RestWithASPNETUdemy Authentication Get User Principal/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,93 @@
+﻿using Tapioca.HATEOAS;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonsController : Controller
+    {
+
+        private IPersonBusiness _personBusiness;
+
+        public PersonsController(IPersonBusiness personBusiness)
+        {
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [SwaggerResponse((200), Type = typeof(List<PersonVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return new OkObjectResult(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerResponse((200), Type = typeof(PersonVO))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return new OkObjectResult(person);
+        }
+
+        [HttpPost]
+        [SwaggerResponse((201), Type = typeof(PersonVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody]PersonVO person)
+        {
+            /* Obtém o id do usuário a partir disso basta fazermos
+            log, auditoria etc
+            */
+            var userName = User.Identity.Name;
+
+            if (person == null) return BadRequest();
+            return new OkObjectResult(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [SwaggerResponse((202), Type = typeof(PersonVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody]PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            var updatedPerson = _personBusiness.Update(person);
+            if (updatedPerson == null) return BadRequest();
+            return new OkObjectResult(updatedPerson);
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        [Authorize("Bearer")]
+        public IActionResult Delete(int id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
