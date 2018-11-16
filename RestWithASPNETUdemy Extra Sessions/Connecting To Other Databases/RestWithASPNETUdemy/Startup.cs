4122391584e93accc561506da0f9da3bd65653c1
leandrocgsi <leandrocgsi@gmail.com>
2018-11-16 14:28:10 -0200

    Adding SQLite connection

--- RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/RestWithASPNETUdemy/Startup.cs
@@ -6,11 +6,24 @@ using Microsoft.Extensions.Logging;
 using RestWithASPNETUdemy.Business;
 using RestWithASPNETUdemy.Business.Implementattions;
 using Microsoft.EntityFrameworkCore;
+
+using DomainModel.Model.Repository;
+
+//Connect to Sqlite
+using DataAccessSqliteProvider.Context;
+using DataAccessSqliteProvider.Repository.Implementattions;
+
+//Connect to MySQL
 //using DataAccessMySqlProvider.Context;
 //using DataAccessMySqlProvider.Repository.Implementattions;
-using DomainModel.Model.Repository;
-using DataAccessPostgreeSQLProvider.Context;
-using DataAccessPostgreeSQLProvider.Repository.Implementattions;
+
+//Connect to Microsoft SQL Server
+//using DataAccessMSSqlServerProvider.Context;
+//using DataAccessMSSqlServerProvider.Repository.Implementattions;
+
+//Connect to Postgree
+//using DataAccessPostgreeSQLProvider.Context;
+//using DataAccessPostgreeSQLProvider.Repository.Implementattions;
 
 namespace RestWithASPNETUdemy
 {
@@ -26,15 +39,21 @@ namespace RestWithASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Using a SQLite database
+            var connectionString = Configuration["SqliteSqlConnection:SqliteSqlConnectionString"];
+            services.AddDbContext<SqliteSQLContext>(options => options.UseSqlite(connectionString));
+
             //Using a MySQL database
             /*var connectionString = Configuration["MySqlConnection:MySqlConnectionString"];
-
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connectionString)); */
 
             //Using a PostgreSQL database
-            var connectionString = Configuration["PostgreeSqlConnection:PostgreeSqlConnectionString"];
+            /*var connectionString = Configuration["PostgreeSqlConnection:PostgreeSqlConnectionString"];
+            services.AddDbContext<PostgreeSQLContext>(options => options.UseNpgsql(connectionString));*/
 
-            services.AddDbContext<PostgreeSQLContext>(options => options.UseNpgsql(connectionString));
+            //Using a Microsoft SQL Server
+            /*var connectionString = Configuration["MSSqlServerConnection:MSSqlServerConnectionString"];
+            services.AddDbContext<MSSQLServerContext>(options => options.UseSqlServer(connectionString));*/
 
             services.AddMvc();
 
