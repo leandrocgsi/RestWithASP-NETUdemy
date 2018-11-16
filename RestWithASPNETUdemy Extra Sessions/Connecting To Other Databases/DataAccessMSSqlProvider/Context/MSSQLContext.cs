41d1e543b41c82d5f1123abb32bbca8f4e336af9
leandrocgsi <leandrocgsi@gmail.com>
2018-11-16 13:44:30 -0200

    Adding MS SQL Server connection

---
+++ RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/DataAccessMSSqlProvider/Context/MSSQLContext.cs
@@ -0,0 +1,12 @@
+﻿using DomainModel.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessMSSqlServerProvider.Context
+{
+    public class MSSQLContext : DbContext
+    {
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options) {}
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
