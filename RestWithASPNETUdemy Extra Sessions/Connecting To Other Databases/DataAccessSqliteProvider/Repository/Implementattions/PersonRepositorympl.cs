0bedb187259abc380df64f3e98f7562789c6d995
leandrocgsi <leandrocgsi@gmail.com>
2018-11-16 13:45:11 -0200

    Adding SQLite connection

---
+++ RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/DataAccessSqliteProvider/Repository/Implementattions/PersonRepositorympl.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using DataAccessSqliteProvider.Context;
+using DomainModel.Model;
+using DomainModel.Model.Repository;
+
+namespace DataAccessSqliteProvider.Repository.Implementattions
+{
+    public class PersonRepositoryImpl : IPersonRepository
+    {
+        private readonly PostgreeSQLContext _context;
+
+        public PersonRepositoryImpl(PostgreeSQLContext context)
+        {
+            _context = context;
+        }
+
+        // Metodo responsável por criar uma nova pessoa
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return person;
+        }
+
+        // Método responsável por retornar uma pessoa
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Person Update(Person person)
+        {
+            // Verificamos se a pessoa existe na base
+            // Se não existir retornamos uma instancia vazia de pessoa
+            if (!Exists(person.Id)) return null;
+
+            // Pega o estado atual do registro no banco
+            // seta as alterações e salva
+            var result = _context.Persons.SingleOrDefault(b => b.Id == person.Id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return result;
+        }
+
+        // Método responsável por deletar
+        // uma pessoa a partir de um ID
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(i => i.Id.Equals(id));
+            try
+            {
+                if (result != null) _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Exists(long? id)
+        {
+            return _context.Persons.Any(b => b.Id.Equals(id));
+        }
+    }
+}
