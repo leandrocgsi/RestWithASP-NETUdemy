9d7d63c4e6876e18a9eb146da6ad7c1fd13f8367
leandrocgsi <leandrocgsi@gmail.com>
2018-11-26 14:45:24 -0200

    Removing dead code

--- RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/DataAccessMSSqlProvider/Repository/Implementattions/PersonRepositorympl.cs
+++ RestWithASPNETUdemy Extra Sessions/Connecting To Other Databases/DataAccessMSSqlProvider/Repository/Implementattions/PersonRepositorympl.cs
@@ -16,10 +16,6 @@ namespace DataAccessMSSqlServerProvider.Repository.Implementattions
             _context = context;
         }
 
-        // Metodo responsável por criar uma nova pessoa
-        // nesse momento adicionamos o objeto ao contexto
-        // e finalmente salvamos as mudanças no contexto
-        // na base de dados
         public Person Create(Person person)
         {
             try
@@ -34,27 +30,21 @@ namespace DataAccessMSSqlServerProvider.Repository.Implementattions
             return person;
         }
 
-        // Método responsável por retornar uma pessoa
         public Person FindById(long id)
         {
             return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
-        // Método responsável por retornar todas as pessoas
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
         }
 
-        // Método responsável por atualizar uma pessoa
         public Person Update(Person person)
         {
-            // Verificamos se a pessoa existe na base
-            // Se não existir retornamos uma instancia vazia de pessoa
+
             if (!Exists(person.Id)) return null;
 
-            // Pega o estado atual do registro no banco
-            // seta as alterações e salva
             var result = _context.Persons.SingleOrDefault(b => b.Id == person.Id);
             if (result != null)
             {
@@ -71,8 +61,6 @@ namespace DataAccessMSSqlServerProvider.Repository.Implementattions
             return result;
         }
 
-        // Método responsável por deletar
-        // uma pessoa a partir de um ID
         public void Delete(long id)
         {
             var result = _context.Persons.SingleOrDefault(i => i.Id.Equals(id));
