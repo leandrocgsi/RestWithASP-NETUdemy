cd55c12c1004628683135594855cfccd0b4f7909
leandrocgsi <leandrocgsi@gmail.com>
2018-12-24 09:07:10 -0200

    Removing comments

--- RestWithASPNETUdemy 20 - Final Release/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy 20 - Final Release/RestWithASPNETUdemy/Startup.cs
@@ -149,7 +149,6 @@ namespace RestWithASPNETUdemy
             }
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(_configuration.GetSection("Logging"));
